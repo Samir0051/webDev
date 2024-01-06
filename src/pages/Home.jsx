d149e5f859038c833ef17b5c322b42a740eb1a12
Samir <samirsanchez036@gmail.com>
2024-01-06 02:21:46 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import './Home.css'
+import image from "../pictures/cool.jpeg"
 
 export default function Home(){
     return (
     <>
     <div class ="ok">
             <p id ="name">this is a picture of a bird! it looks a bit odd, but i'm sure it's nice</p>
-            <img src= "../pictures/cool.jpeg" alt="React Image" />
+            <img src={image} alt="React Image" />
     </div>
     </>
     )
